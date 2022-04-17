9260239b98792187aa433ec951df9743aaa03b5d
hani <mustikahani@yahoo.com>
2022-04-17 23:33:31 +0700

    update

--- main.py
+++ main.py
@@ -2,9 +2,7 @@
 aplikasi deteksi gempa terkini
 modularisasii  dengan function
 """
-
-
-
+from gempa_terkini import ekstraksi_data, tampilkan_data
 
 if __name__ == '__main__':
     print('aplikasi utama')
