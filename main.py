1c477ae474692ecf33281bed582eb38620701ef3
hani <mustikahani@yahoo.com>
2022-04-18 21:26:37 +0700

    2

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 aplikasi deteksi gempa terkini
 modularisasii  dengan function
 """
-from gempa_terkini import ekstraksi_data, tampilkan_data
+import gempa_terkini
 
 if __name__ == '__main__':
     print('aplikasi utama')
-    result = ekstraksi_data()
-    tampilkan_data(result)
+    result = gempa_terkini.ekstraksi_data()
+    gempa_terkini.tampilkan_data(result)
