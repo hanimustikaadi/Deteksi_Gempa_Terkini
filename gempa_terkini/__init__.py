6a7852ad8711600d6811216f0821851615ca4a94
hani <mustikahani@yahoo.com>
2022-04-22 22:26:26 +0700

    2

--- gempa_terkini/__init__.py
+++ gempa_terkini/__init__.py
@@ -1,3 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+
+
 
 
 def ekstraksi_data():
@@ -11,18 +15,32 @@ def ekstraksi_data():
     Dirasakan (Skala MMI): II Kota Sorong
     return :
     """
+    try:
+        content= requests.get('https://www.bmkg.go.id/')
+    except Exception:
+        return None
+    if content.status_code == 200:
+        print(content.status_code)
+    #soup = BeautifulSoup(content)
+    #print(soup.prettify())
 
-    hasil = dict()
-    hasil['tanggal'] = '15 Februari 2022'
-    hasil['waktu']  = '16:51:43 WIB'
-    hasil['magnitudo'] = 3.4
-    hasil['kedalaman'] =  10
-    hasil['lokasi'] = {'LS' : 0.72  , 'BT':  131.51 }
-    hasil['keterangan'] = 'Pusat gempa berada di laut 29 km Timur Laut Kota Sorong'
-    hasil['dirasakan']  = 'Dirasakan (Skala MMI): II Kota Sorong'
-    return hasil
+        hasil = dict()
+        hasil['tanggal'] = '15 Februari 2022'
+        hasil['waktu']  = '16:51:43 WIB'
+        hasil['magnitudo'] = 3.4
+        hasil['kedalaman'] =  10
+        hasil['lokasi'] = {'LS' : 0.72  , 'BT':  131.51 }
+        hasil['keterangan'] = 'Pusat gempa berada di laut 29 km Timur Laut Kota Sorong'
+        hasil['dirasakan']  = 'Dirasakan (Skala MMI): II Kota Sorong'
+        return hasil
+    else:
+        return  None
 
 def tampilkan_data(result):
+
+    if result is None:
+        print('Tidak bisa menemukan data gempa terkini')
+        return
     print('Gempa berdasarkan BMKG')
     print(f'tanggal {result["tanggal"]}')
     print(f'magnitudo {result["magnitudo"]}')
@@ -31,5 +49,5 @@ def tampilkan_data(result):
     print(f'keterangan {result["keterangan"]}')
     print(f'dirasakan {result["dirasakan"]}')
 
-if __name__ == '__main__':
-    print('ini adalah package gempa terkini')
+#if __name__ == '__main__':
+    #print('ini adalah package gempa terkini')
